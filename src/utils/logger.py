f540919afce5d0dd9630cca1eb979d29683b6fcc
Hun-Bot2 <surtrcode@gmail.com>
2026-01-08 19:59:40 +0900

    revise : fix utils

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,9 +1,9 @@
 """
 Logging utilities
 """
+import os
 import logging
 import sys
-from config.settings import LOG_LEVEL, LOG_FORMAT
 
 
 def get_logger(name: str) -> logging.Logger:
@@ -16,13 +16,14 @@ def get_logger(name: str) -> logging.Logger:
     Returns:
         Configured logger
     """
+    log_level = os.getenv("LOG_LEVEL", "INFO")
     logger = logging.getLogger(name)
-    logger.setLevel(getattr(logging, LOG_LEVEL))
+    logger.setLevel(getattr(logging, log_level))
     
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(getattr(logging, LOG_LEVEL))
-        formatter = logging.Formatter(LOG_FORMAT)
+        handler.setLevel(getattr(logging, log_level))
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     
